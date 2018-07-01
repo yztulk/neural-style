93a9cc780bf02d3decead50c80b1e74279843b3a
yztulk <roybout@gmail.com>
2018-07-01 20:29:08 +1000

    Update execution_script_aws.sh

--- execution_script_aws.sh
+++ execution_script_aws.sh
@@ -7,8 +7,8 @@ th ~/neural-style/neural_style.lua \
 -content_image ~/Documents/content/sunset.jpeg \
 -style_image ~/Documents/styles/psychedelic_colours.jpg \
 -output_image ~/Documents/outputs/sunset_psychedelic.jpg \
--num_iterations 1000 \
--image_size 800 \
+-num_iterations 200 \
+-image_size 400 \
 -save_iter 100 \
 -content_layers relu4_2 \
 -style_layers relu1_1,relu2_1,relu3_1,relu4_1,relu5_1 \
@@ -17,6 +17,6 @@ th ~/neural-style/neural_style.lua \
 echo "$(date) all interations completed" >> log.txt
 cd ~/Documents/outputs/
 tar -zcvf results.tar.gz ~/Documents/outputs/
-echo "Results from neural-style" | mutt -a "results.tar.gz" -s "neural-style results" -- [emailaddress]
-echo "$(date) email sent and the system has been shut down" >> log.txt
+echo "Results from neural-style" | mutt -a "results.tar.gz" -s "neural-style results" -- roybout@gmail.com
+#echo "$(date) email sent and the system has been shut down" >> log.txt
 sudo shutdown
